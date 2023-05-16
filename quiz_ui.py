27571bb1d7e1a8361cf77a23e6194c316b3b8882
Angela Kerubo <nkerubo.angela@gmail.com>
2023-05-16 23:07:48 +0300

    Update quiz_ui.py

--- quiz_ui.py
+++ quiz_ui.py
@@ -2,25 +2,41 @@ import json
 import tkinter as tk
 from tkinter import messagebox
 
-
 class QuizGame:
     def __init__(self, master):
         self.master = master
         self.master.title("Quiz Game")
-        self.master.configure(bg="#f2f2f2")  # Set background color
+        self.master.configure(bg="black")  # Set background color
 
         self.quiz_data = self.load_quiz_data("quiz.json")
         self.current_question = 0
         self.score = 0
+        self.user_answers = []
 
-        self.question_label = tk.Label(self.master, text="", wraplength=400, bg="#f2f2f2", font=("Arial", 14, "bold"))
-        self.question_label.pack(pady=20)
+        self.question_label = tk.Label(self.master, text="", wraplength=400, bg="grey", font=("Arial", 16, "bold"))
+        self.question_label.pack(pady=20, fill=tk.BOTH, expand=True)
 
-        self.options_listbox = tk.Listbox(self.master, selectmode=tk.SINGLE, bg="#ffffff", font=("Arial", 12))
-        self.options_listbox.pack(pady=10)
+        self.options_listbox = tk.Listbox(
+            self.master,
+            selectmode=tk.SINGLE,
+            bg="grey",
+            font=("Arial", 14),
+            justify=tk.CENTER,
+            exportselection=False,
+        )
+        self.options_listbox.pack(pady=20, fill=tk.BOTH, expand=True)
 
-        self.next_button = tk.Button(self.master, text="Next", command=self.next_question, bg="#4caf50", fg="#ffffff", font=("Arial", 12, "bold"))
-        self.next_button.pack(pady=10)
+        self.previous_button = tk.Button(
+            self.master, text="Previous", command=self.previous_question, bg="#2196f3", fg="#ffffff", font=("Arial", 12, "bold")
+        )
+        self.previous_button.place(relx=0.4, rely=0.7, anchor='w',)
+
+        self.next_button = tk.Button(
+            self.master, text="Next", command=self.next_question, bg="#4caf50", fg="#ffffff", font=("Arial", 12, "bold")
+        )
+        self.next_button.place(relx=0.6, rely=0.7, anchor='e',)
+
+        self.load_question()
 
     def load_quiz_data(self, file_path):
         with open(file_path) as file:
@@ -35,6 +51,19 @@ class QuizGame:
         for option in question["options"]:
             self.options_listbox.insert(tk.END, option)
 
+        # Select the user's previous answer if available
+        if len(self.user_answers) > self.current_question and self.user_answers[self.current_question] in question["options"]:
+            self.options_listbox.select_set(question["options"].index(self.user_answers[self.current_question]))
+        else:
+            self.options_listbox.selection_clear(0, tk.END)
+
+    def previous_question(self):
+        if self.current_question == 0:
+            return
+
+        self.current_question -= 1
+        self.load_question()
+
     def next_question(self):
         if self.options_listbox.curselection() == ():
             messagebox.showerror("Error", "Please select an answer.")
@@ -44,6 +73,7 @@ class QuizGame:
         question = self.quiz_data["questions"][self.current_question]
         if user_answer.lower() == question["answer"].lower():
             self.score += 1
+        self.user_answers.append(user_answer)
 
         self.current_question += 1
 
@@ -53,7 +83,13 @@ class QuizGame:
             self.show_result()
 
     def show_result(self):
-        messagebox.showinfo("Quiz Result", f"You've completed the quiz!\nYour score: {self.score}")
+        result_message = f"You've completed the quiz!\nYour score: {self.score}/{len(self.quiz_data['questions'])}\n\n"
+        for i, question in enumerate(self.quiz_data["questions"]):
+            result_message += f"Question {i + 1}: {question['question']}\n"
+            result_message += f"Your answer: {self.user_answers[i]}\n"
+            result_message += f"Correct answer: {question['answer']}\n\n"
+
+        messagebox.showinfo("Quiz result", result_message)
 
 
 if __name__ == "__main__":
